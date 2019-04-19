d26b31309de42eded36d62b91841c12e0dafa92a
Craig Berry <cberry.swe@gmail.com>
2019-04-18 22:16:47 -0400

    more mongo stuff

--- src/main/kotlin/cberry/dev/controllers/HomeController.kt
+++ src/main/kotlin/cberry/dev/controllers/HomeController.kt
@@ -1,11 +1,19 @@
 package cberry.dev.controllers
 
+import com.mongodb.reactivestreams.client.MongoClient
 import io.micronaut.http.annotation.Controller
 import io.micronaut.http.annotation.Get
+import javax.inject.Inject
 
 @Controller("/")
-class HomeController {
+class HomeController(
+    @Inject val mongoClient: MongoClient
+) {
 
     @Get
     fun home() = "Hello Worldz"
+
+    @Get("/db")
+    fun db() = mongoClient.listDatabaseNames()
+
 }
