7d2a0e9d870cbdb9c7d0ac90844842591177877f
Craig Berry <cberry.swe@gmail.com>
2019-04-21 16:55:35 -0400

    connect to mongo in controller

--- src/main/kotlin/cberry/dev/controllers/HomeController.kt
+++ src/main/kotlin/cberry/dev/controllers/HomeController.kt
@@ -3,6 +3,7 @@ package cberry.dev.controllers
 import com.mongodb.reactivestreams.client.MongoClient
 import io.micronaut.http.annotation.Controller
 import io.micronaut.http.annotation.Get
+import io.reactivex.Single
 import javax.inject.Inject
 
 @Controller("/")
@@ -14,6 +15,24 @@ class HomeController(
     fun home() = "Hello Worldz"
 
     @Get("/db")
-    fun db() = mongoClient.listDatabaseNames()
+    fun db(): Single<Long> {
+        println("db version 1")
+        return Single.fromPublisher(
+            mongoClient.getDatabase("admin")
+                .getCollection("test")
+                .count()
+        )
+
+//        val db = mongoClient.getDatabase("admin")
+//        println("db found: $db")
+//
+//        val collection = db.getCollection("test")
+//        println("'test' collection found: $collection")
+//
+//        val count = collection.count()
+//        println("count found: $count")
+//
+//        return "test"
+    }
 
 }
